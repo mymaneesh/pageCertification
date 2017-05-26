cc4d8454b1c994c2933790d8350f66db2cb0eea7
Maneesh P <maneesh.p@outlook.com>
2017-05-27 00:05:54 +0530

    Add new js file

---
+++ pageCertifyList.js
@@ -0,0 +1,21 @@
+module.exports = {
+	"network_hierarchy": {
+		"label": "Network Hierarchy",
+		"description": "Good all",
+		"status": "verified"
+	},
+
+	"network_settings": {
+		"label": "Network Settings",
+		"description": "Some eror",
+		"status": "error"
+	},
+
+	"deviceCredentials": {
+		"label": "Device Credentials",
+		"description": "Some eror",
+		"status": "warning"
+	}
+	
+
+};
